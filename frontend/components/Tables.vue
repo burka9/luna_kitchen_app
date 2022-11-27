465a7e68e1bde5c3537967675c77181d7da27006
Biruk Ephrem <birukeph@gmail.com>
2022-11-28 01:15:42 +0300

    added table order UI

--- frontend/components/Tables.vue
+++ frontend/components/Tables.vue
@@ -29,11 +29,25 @@ const removeTable = table => {
 		.catch(err => console.log(err))
 }
 
+const fetch_orders = () => {
+	axios.get(`${props.api}/api/order?status=pending`)
+		.then(result => {
+			if (result.data.success)
+				result.data.list.forEach(order => {
+					let index = state.tables.findIndex(table => table.table_index == order.table)
+					state.tables[index]['order'] = order
+				})
+		})
+		.catch(err => console.error(err))
+}
+
 const fetch_list = () => {
 	axios.get(`${props.api}/api/table?sort=true`)
 		.then(result => {
 			if (result.data.success) {
+				console.log(result.data.list)
 				state.tables = result.data.list
+				fetch_orders()
 			}
 		})
 		.catch(err => console.log(err))
@@ -47,14 +61,23 @@ fetch_list()
 		<v-row align="center">
 			<v-col cols="12" sm="6" md="4" lg="3" v-for="(table, index) in state.tables" :key="index">
 				<v-card elevation="3">
-					<v-card-title>Table {{ table.table_index }}</v-card-title>
-					<v-card-actions>
-						<v-btn color="error" depressed dense @click="removeTable(table)">Remove</v-btn>
-					</v-card-actions>
+					<v-card-text>
+						<v-row>
+							<v-col cols="12" md="4">
+								<p class="text-h6">Table {{ table.table_index }}</p>
+								<v-btn color="error" depressed small dense @click="removeTable(table)">Remove</v-btn>
+							</v-col>
+							<v-col cols="12" md="8" v-if="table.order">
+								<p><b>Issued by:</b> {{ table.order.user.name }}</p>
+								<p><b>Orders:</b> {{ table.order.items.map(item => item.name).join(', ') }}</p>
+							</v-col>
+						</v-row>
+					</v-card-text>
 				</v-card>
 			</v-col>
 			<v-col cols="12" sm="6" md="4" lg="3" align="center" fill-height>
-				<v-btn fab color="primary" :disabled="state.adding" @click="addTable">
+				<v-btn color="primary" :disabled="state.adding" @click="addTable">
+					<span class="mr-3">Add Table</span>
 					<v-icon>mdi-plus</v-icon>
 				</v-btn>
 			</v-col>
